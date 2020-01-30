e99b81585f412e53b6cc2c3fd8c0076440e8baec
artemvasiltsov <artemvasiltsov01@gmail.com>
2020-01-30 14:44:54 +0200

    Update

--- vehicle_detection/main_detection.py
+++ vehicle_detection/main_detection.py
@@ -15,7 +15,7 @@ class MainDetection:
     def main_detect(self):
 
         vehicle_image_paths = glob.glob(os.path.join(self.input_dir, "*.jpg"))
-
+        index = 0
         for vehicle_image_path in vehicle_image_paths:
 
-            self.sub_detection.detect_cars_image(frame_path=vehicle_image_path)
+            index = self.sub_detection.detect_cars_image(frame_path=vehicle_image_path, idx=index)
--- vehicle_detection/sub_detection.py
+++ vehicle_detection/sub_detection.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-from settings import VEHICLE_LABEL, CUR_DIR, OUTPUT_DIR
+from settings import VEHICLE_LABEL, CUR_DIR, OUTPUT_DIR, IMAGE_MAX_HEIGHT, IMAGE_MAX_WIDTH
 
 
 class SubDetection:
@@ -12,7 +12,7 @@ class SubDetection:
         self.graph_path = os.path.join(CUR_DIR, 'utils', 'frcnn_inception_v2_graph.pbtxt')
         self.output_dir = OUTPUT_DIR
 
-    def detect_cars_image(self, frame_path):
+    def detect_cars_image(self, frame_path, idx):
 
         cvNet = cv2.dnn.readNetFromTensorflow(self.model_path, self.graph_path)
 
@@ -22,21 +22,41 @@ class SubDetection:
         cvNet.setInput(cv2.dnn.blobFromImage(frame, size=(300, 300), swapRB=True, crop=False))
         cvOut = cvNet.forward()
 
-        index = 0
+        index = idx
         for detection in cvOut[0, 0, :, :]:
 
             score = float(detection[2])
             label = int(detection[1])
             if label in VEHICLE_LABEL and score > 0.3:
 
-                left = detection[3] * frame_width
-                top = detection[4] * frame_height
-                right = detection[5] * frame_width
-                bottom = detection[6] * frame_height
+                left = int(detection[3] * frame_width)
+                top = int(detection[4] * frame_height)
+                right = int(detection[5] * frame_width)
+                bottom = int(detection[6] * frame_height)
 
                 vehicle_img = frame[top:bottom, left:right]
+                vehicle_image_width = vehicle_img.shape[1]
+                vehicle_image_height = vehicle_img.shape[0]
+
+                if vehicle_image_width > IMAGE_MAX_WIDTH or vehicle_image_height > IMAGE_MAX_HEIGHT:
+
+                    resize_ratio = 1
+                    if vehicle_image_width > IMAGE_MAX_WIDTH:
+
+                        resize_ratio = IMAGE_MAX_WIDTH / vehicle_image_width
+                    elif vehicle_image_height > IMAGE_MAX_HEIGHT:
+
+                        resize_ratio = IMAGE_MAX_HEIGHT / vehicle_image_height
+
+                    resized_vehicle_image = cv2.resize(vehicle_img, None, fx=resize_ratio, fy=resize_ratio,
+                                                       interpolation=cv2.INTER_CUBIC)
+                else:
+
+                    resized_vehicle_image = vehicle_img
+
                 vehicle_img_path = os.path.join(self.output_dir, 'image_{}.jpg'.format(index))
-                cv2.imwrite(vehicle_img_path, vehicle_img)
+                cv2.imwrite(vehicle_img_path, resized_vehicle_image)
+                print("Saved {}".format(vehicle_img_path))
                 index += 1
 
-        return
+        return index
